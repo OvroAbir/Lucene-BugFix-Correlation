2794c3717128c8722414d591ef8f62aac7bdc2e6
Joy Ghosh <joyghosh826@gmail.com>
2020-05-02 20:30:54 -0600

    changed main file to run code

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,11 @@
 from src.jira.jira_rest_request import Jira_Rest_Request
 
 
+from src.jira.jira import Jira
+from src.jira.jira_rest_request import Jira_Rest_Request
+
 jira_url = Jira_Rest_Request.get_jira_rest_url("Closed", 10)
-print(jira_url)
-# jira = Jira(jira_url, "../data/lucene-closed-data-10.json", "lucene")
+jira = Jira(jira_url, "../data/lucene-closed-data-10.json", "lucene")
+
+print("jira url: ", jira_url)
+print("Jira Object:", jira)
